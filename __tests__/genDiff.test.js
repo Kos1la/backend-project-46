bacb8fa221b7e1e825011e19ed42667cd470728a
Kos1la <kos1la1998@gmail.com>
2024-09-24 13:30:36 +0300

    added test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,3 +1,18 @@
 import genDiff from '../src/genDiff';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import path from 'path';
 
-test('genDiff', () => {});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('genDiff json files', () => {
+  const file1 = readFile('file1.json');
+  const file2 = readFile('file2.json');
+  const expected = readFile('expected.txt').trim();
+
+  expect(genDiff(JSON.parse(file1), JSON.parse(file2))).toBe(expected);
+});
--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,14 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-export default [{ languageOptions: { globals: globals.node } }, pluginJs.configs.recommended];
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node, // Это добавляет все глобальные переменные для Node.js
+        ...globals.jest, // Это добавляет глобальные переменные для Jest
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
