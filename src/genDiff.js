cedd797afbb4b015b1fcdab22d2711eac9f3feab
Kos1la <kos1la1998@gmail.com>
2024-09-24 20:03:36 +0300

    fixed genDiff Cognitive Complexity

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,20 +1,26 @@
 import _ from 'lodash';
 
+const formatRemoved = (key, value) => `  - ${key}: ${value}`;
+const formatAdded = (key, value) => `  + ${key}: ${value}`;
+const formatUnchanged = (key, value) => `    ${key}: ${value}`;
+const formatChanged = (key, value1, value2) =>
+  `${formatRemoved(key, value1)}\n${formatAdded(key, value2)}`;
+
 const genDiff = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
   const sortedKeys = _.sortBy(keys);
 
   const result = sortedKeys.map((key) => {
     if (!_.has(data2, key)) {
-      return `  - ${key}: ${data1[key]}`;
+      return formatRemoved(key, data1[key]);
     }
     if (!_.has(data1, key)) {
-      return `  + ${key}: ${data2[key]}`;
+      return formatAdded(key, data2[key]);
     }
     if (!_.isEqual(data1[key], data2[key])) {
-      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+      return formatChanged(key, data1[key], data2[key]);
     }
-    return `    ${key}: ${data1[key]}`;
+    return formatUnchanged(key, data1[key]);
   });
 
   return `{\n${result.join('\n')}\n}`;
