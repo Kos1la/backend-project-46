bba8bd57ec44edff9e87f27805c04f41bf1782b2
kos1la <kos1la1998@gmail.com>
2024-10-28 12:23:35 +0300

    add lint again

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,7 @@
 import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-export default [{ languageOptions: { globals: globals.browser } }];
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+];
